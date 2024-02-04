40f3c1a19b033d2c46e7c0fdc619faf31b7aed41
Heeps10 <thomasheeps10@gmail.com>
2024-02-04 17:37:32 -0500

    Expanded on views.search function to take form input and render it with html template.

--- search/views.py
+++ search/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 
-def attribute(request):
-    return render(request, 'reviewbot.html', {})
+def search(request):    
+    if request.method == "POST":
+        item = forms.ItemForm(request.POST)
+
+        if item.is_valid():
+            input = item.cleaned_data['item']
+            print(input)
+            return render(request, 'reviewbot.html', {'input':input})
+    else:
+        item = forms.ItemForm()
+        return render(request, 'reviewbot.html', {'item':item})
